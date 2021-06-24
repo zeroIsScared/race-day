39a45b82bd5558a236eebf0e70e7fa2e726f3599
zeroIsScared <bujornathalia@gmail.com>
2021-06-24 12:36:58 +0300

    Commiting the Race Day project to

---
+++ main.js
@@ -0,0 +1,23 @@
+//Assigning a random number to runners
+let raceNumber = Math.floor(Math.random(1)*1001);
+console.log(raceNumber);
+//Early registered runners
+let earlyRegistred = true;
+//Runners age
+let runnersAge = 17;
+//Checking if runners are adults
+runnersAge > 18 && earlyRegistred  ? raceNumber += 1000 : raceNumber;
+console.log(raceNumber);
+//Checking and displying age and registration time
+if (runnersAge > 18 && earlyRegistred) { 
+console.log(`The race will start at 9:30 am. Your race is:
+ number ${raceNumber}`);
+} else if (runnersAge > 18 && earlyRegistred === false) {
+    console.log(`The race will start at 11:am. Your race is:
+     number ${raceNumber}`);
+}else if (runnersAge < 18) {
+    console.log(`The race will start at 12:30 am. Your race
+     number is: ${raceNumber}`);
+} else {
+    console.log('Please contact the organisers to find out your running time and race number.');
+    };
